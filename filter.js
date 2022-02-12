328eb787b933b1fc37ed56f0b4f5966090b6ce5a
gangster-blezer <98465840+gangster-blezer@users.noreply.github.com>
2022-02-12 17:36:11 +0300

    Add files via upload

--- filter.js
+++ filter.js
@@ -2,3 +2,12 @@ function menu(el,add,del) {
 	document.getElementsByClassName(el)[0].classList.add(add);
 	document.getElementsByClassName(el)[0].classList.remove(del);
 }
+
+
+document.getElementById("price-range").addEventListener("input", function() {
+  document.getElementById("price-max").value = this.value;
+});
+
+document.getElementById("price-max").addEventListener("input", function() {
+  document.getElementById("price-range").value = this.value;
+});
