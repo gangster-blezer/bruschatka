12c0bd570861061ab4c36ef615120cad1c24ffb9
gangster-blezer <98465840+gangster-blezer@users.noreply.github.com>
2022-02-13 02:24:31 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,30 @@
+var menu_open = false;
+
+function change(el,add,del) {
+	document.getElementsByClassName(el)[0].classList.add(add);
+	document.getElementsByClassName(el)[0].classList.remove(del);
+}
+
+function openMenu(){
+	document.getElementsByClassName("menu-btn")[0].classList.add("menu-btn-close");
+	change("mobile-menu","menu-open","menu-close");
+	change("overlay","overlay-open","overlay-close");
+	document.body.classList.add("body-fixed");
+	menu_open = true;
+}
+
+function closeMenu(){
+	document.getElementsByClassName("menu-btn")[0].classList.remove("menu-btn-close");
+	change("mobile-menu","menu-close","menu-open");
+	change("overlay","overlay-close","overlay-open");
+	document.body.classList.remove("body-fixed");
+	menu_open = false;
+}
+
+document.getElementById("mobile-menu").onclick = function(){
+	if(menu_open){
+		closeMenu();
+	}else{
+		openMenu();
+	}
+}
