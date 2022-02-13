5c080328d3006291cc2c9fd2b0959b00c53d73f9
gangster-blezer <98465840+gangster-blezer@users.noreply.github.com>
2022-02-14 00:31:39 +0300

    Add files via upload

--- filter.js
+++ filter.js
@@ -1,6 +1,28 @@
-function menu(el,add,del) {
+var filter_open = false;
+
+function change(el,add,del) {
 	document.getElementsByClassName(el)[0].classList.add(add);
 	document.getElementsByClassName(el)[0].classList.remove(del);
+}
+
+function openFilter(){
+	change("filter","filter-open","filter-close");
+	document.body.classList.add("body-fixed");
+	filter_open = true;
+}
+
+function closeFilter(){
+	change("filter","filter-close","filter-open");
+	document.body.classList.remove("body-fixed");
+	filter_open = false;
+}
+
+document.getElementById("filter_btn").onclick = function(){
+	if(filter_open){
+		closeFilter();
+	}else{
+		openFilter();
+	}
 }
 
 
